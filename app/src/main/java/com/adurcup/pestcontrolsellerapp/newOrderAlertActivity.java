b57e97c04387a22af61bf454c39d7c058075596a
kshivang <shivang.iitk@gmail.com>
2016-07-20 23:00:04 +0530

    Initial commit

---
+++ app/src/main/java/com/adurcup/pestcontrolsellerapp/newOrderAlertActivity.java
@@ -0,0 +1,17 @@
+package com.adurcup.pestcontrolsellerapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kshivang on 20/07/16.
+ * This activity is for Alert Notification with alert sound
+ */
+public class newOrderAlertActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_order_aleart);
+    }
+}
